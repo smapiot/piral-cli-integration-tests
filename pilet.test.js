c89762a91700a97eceb8393509dc75586666a43c
Florian Rappl <florian.rappl@smapiot.com>
2020-12-10 00:10:20 +0100

    Use legacy peer deps with npm v7

--- pilet.test.js
+++ pilet.test.js
@@ -16,109 +16,127 @@ fsPromises.rm = fsPromises.rm || promisify(fs.unlink);
 
 const srcFilePath = path.resolve(process.cwd(), "pilet", "src", "index.tsx");
 const timeoutCommand = type().startsWith("Linux") ? "timeout 60s " : "";
+const cliVersion = process.env.CLI_VERSION || "latest";
+const installFlag = process.version.startsWith("v15")
+  ? "--legacy-peer-deps -- "
+  : "";
 
 jest.setTimeout(300 * 1000); // 300 second timeout
 
 const serverHasStarted = (resolve, port, timeout, ref) => (data) => {
-    if (data.toString().includes(`Running at http://localhost:${port}`)) {
-        clearTimeout(timeout);
-        ref.unsubscribe();
-        resolve();
-    }
+  if (data.toString().includes(`Running at http://localhost:${port}`)) {
+    clearTimeout(timeout);
+    ref.unsubscribe();
+    resolve();
+  }
 };
 
 const waitForRunning = (debugProcess, port) => {
-    return new Promise((resolve, reject) => {
-        timeout = setTimeout(() => reject(new Error("Server not started after 180s")), 180 * 1000);
-        const ref = {};
-        const onData = serverHasStarted(resolve, port, timeout, ref);
-        debugProcess.stdout.on("data", onData);
-        ref.unsubscribe = () => debugProcess.stdout.off("data", onData);
-    });
+  return new Promise((resolve, reject) => {
+    timeout = setTimeout(
+      () => reject(new Error("Server not started after 180s")),
+      180 * 1000
+    );
+    const ref = {};
+    const onData = serverHasStarted(resolve, port, timeout, ref);
+    debugProcess.stdout.on("data", onData);
+    ref.unsubscribe = () => debugProcess.stdout.off("data", onData);
+  });
 };
 
 describe("pilet", () => {
-    it("scaffold pilet", async () => {
-        // TODO: npm list | tail -n +2 > package.list &&
-        await rimraf("pilet-build");
-        await fsPromises.mkdir("pilet-build");
+  it("scaffold pilet", async () => {
+    // TODO: npm list | tail -n +2 > package.list &&
+    await rimraf("pilet-build");
+    await fsPromises.mkdir("pilet-build");
 
-        const info = await execute(`npm init pilet ${process.version.startsWith("v15") ? "-- " : ""}-y`, {
-            cwd: path.resolve(process.cwd(), "pilet-build"),
-        });
+    const info = await execute(
+      `npm init pilet@${cliVersion} ${installFlag}-y`,
+      {
+        cwd: path.resolve(process.cwd(), "pilet-build"),
+      }
+    );
 
-        await rimraf(path.resolve("pilet-build", "node_modules"));
-        await rimraf(path.resolve("pilet-build", "package-lock.json"));
+    await rimraf(path.resolve("pilet-build", "node_modules"));
+    await rimraf(path.resolve("pilet-build", "package-lock.json"));
 
-        expect(info.stderr).toBe("");
+    expect(info.stderr).toBe("");
 
-        const pathToBuildDir = path.resolve(process.cwd(), "pilet-build");
-        expect(pathToBuildDir).toMatchFilesystemSnapshot();
+    const pathToBuildDir = path.resolve(process.cwd(), "pilet-build");
+    expect(pathToBuildDir).toMatchFilesystemSnapshot();
+  });
+
+  it("pilet scaffold with piral source", async () => {
+    await rimraf("pilet");
+    await fsPromises.mkdir("pilet");
+
+    // scaffold new pilet
+    const info = await execute(
+      `npm init pilet@${cliVersion} ${installFlag}--source sample-piral@${cliVersion} -y`,
+      {
+        cwd: path.resolve(process.cwd(), "pilet"),
+      }
+    );
+    expect(info.stderr).toBe("");
+  });
+
+  it("HMR", async (done) => {
+    const port = 38080;
+
+    // start the debug process and wait until compiled and server running
+    const debugProcess = spawn(
+      `${timeoutCommand}npx pilet debug ${
+        process.version.startsWith("v15") ? "-- " : ""
+      }--port ${port}`,
+      {
+        cwd: path.resolve(process.cwd(), "pilet"),
+        shell: true,
+      }
+    );
+    const handleError = jest.fn();
+    debugProcess.stderr.once("data", handleError);
+    await waitForRunning(debugProcess, port);
+
+    // Go to page and check for expected pilet
+    await page.goto(`http://localhost:${port}`, { waitUntil: "networkidle0" });
+    let innerHtml = await page.$eval(".pi-tile", (element) => {
+      return element.children[0].innerHTML;
     });
+    expect(innerHtml).toBe("Welcome to Piral!");
 
-    it("pilet scaffold with piral source", async () => {
-        await rimraf("pilet");
-        await fsPromises.mkdir("pilet");
+    // Will be resolved 1s after the dev server has recompiled the changes
+    const backendReloaded = new Promise((resolve, reject) => {
+      timeout = setTimeout(
+        () => reject(new Error("Server not started after 60s")),
+        60 * 1000
+      );
+      debugProcess.stdout.once("data", () => {
+        clearTimeout(timeout);
+        resolve();
+      });
+    }).then(() => sleep(1000));
 
-        // scaffold new pilet
-        const info = await execute(
-            `npm init pilet@next ${process.version.startsWith("v15") ? "-- " : ""}--source sample-piral@next -y`,
-            {
-                cwd: path.resolve(process.cwd(), "pilet"),
-            }
-        );
-        expect(info.stderr).toBe("");
+    // Update in src/index.tsx to trigger HMR
+    const newString = `Welcome to Test${Math.floor(Math.random() * 10000)}!`;
+    const indexFile = await readFile(srcFilePath);
+    await writeFile(
+      srcFilePath,
+      indexFile.toString().replace("Welcome to Piral!", newString)
+    );
+
+    // Wait until changes have been applied and check the updated pilet
+    await backendReloaded;
+    innerHtml = await page.$eval(".pi-tile", (element) => {
+      return element.children[0].innerHTML;
     });
+    expect(innerHtml).toBe(newString);
 
-    it("HMR", async (done) => {
-        const port = 38080;
+    // Validate, that the debug process did not output any errors and terminate the process
+    expect(handleError).not.toBeCalled();
+    debugProcess.kill("SIGTERM");
+    debugProcess.stdout.destroy();
+    debugProcess.stderr.destroy();
 
-        // start the debug process and wait until compiled and server running
-        const debugProcess = spawn(
-            `${timeoutCommand}npx pilet debug ${process.version.startsWith("v15") ? "-- " : ""}--port ${port}`,
-            {
-                cwd: path.resolve(process.cwd(), "pilet"),
-                shell: true,
-            }
-        );
-        const handleError = jest.fn();
-        debugProcess.stderr.once("data", handleError);
-        await waitForRunning(debugProcess, port);
-
-        // Go to page and check for expected pilet
-        await page.goto(`http://localhost:${port}`, { waitUntil: "networkidle0" });
-        let innerHtml = await page.$eval(".pi-tile", (element) => {
-            return element.children[0].innerHTML;
-        });
-        expect(innerHtml).toBe("Welcome to Piral!");
-
-        // Will be resolved 1s after the dev server has recompiled the changes
-        const backendReloaded = new Promise((resolve, reject) => {
-            timeout = setTimeout(() => reject(new Error("Server not started after 60s")), 60 * 1000);
-            debugProcess.stdout.once("data", () => {
-                clearTimeout(timeout);
-                resolve();
-            });
-        }).then(() => sleep(1000));
-
-        // Update in src/index.tsx to trigger HMR
-        const newString = `Welcome to Test${Math.floor(Math.random() * 10000)}!`;
-        const indexFile = await readFile(srcFilePath);
-        await writeFile(srcFilePath, indexFile.toString().replace("Welcome to Piral!", newString));
-
-        // Wait until changes have been applied and check the updated pilet
-        await backendReloaded;
-        innerHtml = await page.$eval(".pi-tile", (element) => {
-            return element.children[0].innerHTML;
-        });
-        expect(innerHtml).toBe(newString);
-
-        // Validate, that the debug process did not output any errors and terminate the process
-        expect(handleError).not.toBeCalled();
-        debugProcess.kill("SIGTERM");
-        debugProcess.stdout.destroy();
-        debugProcess.stderr.destroy();
-
-        done();
-    });
+    done();
+  });
 });
--- piral.test.js
+++ piral.test.js
@@ -8,27 +8,35 @@ const rimraf = promisify(require("rimraf"));
 const execute = promisify(exec);
 const fsPromises = fs.promises;
 fsPromises.rm = fsPromises.rm || promisify(fs.unlink);
-
 expect.extend({ toMatchFilesystemSnapshot });
+
+const cliVersion = process.env.CLI_VERSION || "latest";
+const installFlag = process.version.startsWith("v15")
+  ? "--legacy-peer-deps -- "
+  : "";
+
 jest.setTimeout(120 * 1000); // 60 second timeout
 
 describe("piral", () => {
-    it("scaffold piral", async () => {
-        // TODO: npm list | tail -n +2 > package.list &&
+  it("scaffold piral", async () => {
+    // TODO: npm list | tail -n +2 > package.list &&
 
-        await rimraf("piral-inst");
-        await fsPromises.mkdir("piral-inst");
+    await rimraf("piral-inst");
+    await fsPromises.mkdir("piral-inst");
 
-        const info = await execute(`npm init piral-instance ${process.version.startsWith("v15") ? "-- " : ""}-y`, {
-            cwd: path.resolve(process.cwd(), "piral-inst"),
-        });
+    const info = await execute(
+      `npm init piral-instance@${cliVersion} ${installFlag}--tag ${cliVersion} -y`,
+      {
+        cwd: path.resolve(process.cwd(), "piral-inst"),
+      }
+    );
 
-        await rimraf(path.resolve("piral-inst", "node_modules"));
-        await fsPromises.rm(path.resolve("piral-inst", "package-lock.json"));
+    await rimraf(path.resolve("piral-inst", "node_modules"));
+    await fsPromises.rm(path.resolve("piral-inst", "package-lock.json"));
 
-        expect(info.stderr).toBe("");
+    expect(info.stderr).toBe("");
 
-        const pathToBuildDir = path.resolve(process.cwd(), "piral-inst");
-        expect(pathToBuildDir).toMatchFilesystemSnapshot();
-    });
+    const pathToBuildDir = path.resolve(process.cwd(), "piral-inst");
+    expect(pathToBuildDir).toMatchFilesystemSnapshot();
+  });
 });
