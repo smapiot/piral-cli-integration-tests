81606d9c08694be47f3d9f70351339e8b436c91b
Majd-sufian <majdnewsufian@gmail.com>
2022-03-09 15:21:52 +0100

    Added More Tests for Piral Declaration

--- src/piral-validate.test.ts
+++ src/piral-validate.test.ts
@@ -5,31 +5,21 @@ runTests('piral-validate', ({ test, setup }) => {
     await ctx.run(`npx --package piral-cli@${cliVersion} piral new --bundler none`);
   });
 
-  test(
-    'validate-standard-template',
-    'standard template should be valid',
-    [],
-    async (ctx) => {
-      const result = await ctx.run(`npx piral validate`);
+  test('validate-standard-template', 'standard template should be valid', [], async (ctx) => {
+    const result = await ctx.run(`npx piral validate`);
 
-      //TODO this is an issue right now
-      //expect(result).toContain('Validation successful. No errors or warnings.');
-      expect(result).toContain('Validation succeeded with 9 warning(s).');
-    },
-  );
+    //TODO this is an issue right now
+    //expect(result).toContain('Validation successful. No errors or warnings.');
+    expect(result).toContain('Validation succeeded with 9 warning(s).');
+  });
 
-  test(
-    'validate-inconsistent-piral-cli',
-    'inconsistent piral CLI should yield warning',
-    [],
-    async (ctx) => {
-      await ctx.run('npm i piral-cli@0.14.3');
+  test('validate-inconsistent-piral-cli', 'inconsistent piral CLI should yield warning', [], async (ctx) => {
+    await ctx.run('npm i piral-cli@0.14.3');
 
-      const result = await ctx.run(`npx piral validate`);
+    const result = await ctx.run(`npx piral validate`);
 
-      //TODO this is an issue right now
-      //expect(result).toContain('Validation succeeded with 1 warning(s)');
-      expect(result).toContain('Validation succeeded with 9 warning(s)');
-    },
-  );
+    //TODO this is an issue right now
+    //expect(result).toContain('Validation succeeded with 1 warning(s)');
+    expect(result).toContain('Validation succeeded with 9 warning(s)');
+  });
 });
