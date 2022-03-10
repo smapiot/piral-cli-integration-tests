adcf72c6c4fd427615be35eee9df133595c74303
Majd-sufian <majdnewsufian@gmail.com>
2022-03-10 13:13:57 +0100

    Added More Tests for Pilet Debug

--- src/pilet-debug.test.ts
+++ src/pilet-debug.test.ts
@@ -8,199 +8,199 @@ runTests('pilet-debug', ({ test, setup }) => {
     await ctx.run(`npm i emojis-list@3.0.0`);
   });
 
-  // test('debug-standard-template', 'can produce a debug build', ['debug.pilet'], async (ctx) => {
-  //   const port = await getFreePort(1256);
-  //   const cp = ctx.runAsync(`npx pilet debug --port ${port}`);
+  test('debug-standard-template', 'can produce a debug build', ['debug.pilet'], async (ctx) => {
+    const port = await getFreePort(1256);
+    const cp = ctx.runAsync(`npx pilet debug --port ${port}`);
 
-  //   await cp.waitUntil('Ready', 'The bundling process failed');
+    await cp.waitUntil('Ready', 'The bundling process failed');
 
-  //   await page.goto(`http://localhost:${port}`);
+    await page.goto(`http://localhost:${port}`);
 
-  //   await expect(page).toHaveSelectorCount('.pi-tile', 1);
+    await expect(page).toHaveSelectorCount('.pi-tile', 1);
 
-  //   await expect(page).toMatchText('.pi-tile', 'Welcome to Piral!');
-  // });
+    await expect(page).toMatchText('.pi-tile', 'Welcome to Piral!');
+  });
 
-  // test('reload-standard-template', 'can reload when developing a debug build', ['debug.pilet'], async (ctx) => {
-  //   const port = await getFreePort(1257);
-  //   const cp = ctx.runAsync(`npx pilet debug --port ${port}`);
+  test('reload-standard-template', 'can reload when developing a debug build', ['debug.pilet'], async (ctx) => {
+    const port = await getFreePort(1257);
+    const cp = ctx.runAsync(`npx pilet debug --port ${port}`);
 
-  //   await cp.waitUntil('Ready', 'The bundling process failed');
+    await cp.waitUntil('Ready', 'The bundling process failed');
 
-  //   await page.goto(`http://localhost:${port}`);
+    await page.goto(`http://localhost:${port}`);
 
-  //   await ctx.setFiles({
-  //     'src/index.tsx'(content: string) {
-  //       return content.replace('<div>Welcome to Piral!</div>', "<div className='foobar'>Welcome to Foo...</div>");
-  //     },
-  //   });
+    await ctx.setFiles({
+      'src/index.tsx'(content: string) {
+        return content.replace('<div>Welcome to Piral!</div>', "<div className='foobar'>Welcome to Foo...</div>");
+      },
+    });
 
-  //   await page.waitForSelector('.foobar');
+    await page.waitForSelector('.foobar');
 
-  //   await expect(page).toHaveSelectorCount('.pi-tile', 1);
+    await expect(page).toHaveSelectorCount('.pi-tile', 1);
 
-  //   await expect(page).toMatchText('.pi-tile', 'Welcome to Foo...');
-  // });
+    await expect(page).toMatchText('.pi-tile', 'Welcome to Foo...');
+  });
 
-  // test(
-  //   'debug-standard-template-with-schema-v0',
-  //   'can produce a debug build with schema v0',
-  //   ['debug.pilet'],
-  //   async (ctx) => {
-  //     const port = await getFreePort(1256);
-  //     const cp = ctx.runAsync(`npx pilet debug --port ${port} --schema v0`);
+  test(
+    'debug-standard-template-with-schema-v0',
+    'can produce a debug build with schema v0',
+    ['debug.pilet'],
+    async (ctx) => {
+      const port = await getFreePort(1256);
+      const cp = ctx.runAsync(`npx pilet debug --port ${port} --schema v0`);
 
-  //     await cp.waitUntil('Ready', 'The bundling process failed');
+      await cp.waitUntil('Ready', 'The bundling process failed');
 
-  //     await page.goto(`http://localhost:${port}`);
+      await page.goto(`http://localhost:${port}`);
 
-  //     const res = await axios.get(`http://localhost:${port}/$pilet-api`);
-  //     const pilets = res.data;
+      const res = await axios.get(`http://localhost:${port}/$pilet-api`);
+      const pilets = res.data;
 
-  //     expect(pilets).toEqual({
-  //       name: expect.anything(),
-  //       version: expect.anything(),
-  //       link: expect.anything(),
-  //       spec: 'v0',
-  //       hash: expect.anything(),
-  //       noCache: expect.anything(),
-  //     });
+      expect(pilets).toEqual({
+        name: expect.anything(),
+        version: expect.anything(),
+        link: expect.anything(),
+        spec: 'v0',
+        hash: expect.anything(),
+        noCache: expect.anything(),
+      });
 
-  //     await expect(page).toHaveSelectorCount('.pi-tile', 1);
+      await expect(page).toHaveSelectorCount('.pi-tile', 1);
 
-  //     await expect(page).toMatchText('.pi-tile', 'Welcome to Piral!');
-  //   },
-  // );
+      await expect(page).toMatchText('.pi-tile', 'Welcome to Piral!');
+    },
+  );
 
-  // test(
-  //   'debug-standard-template-with-schema-v1',
-  //   'can produce a debug build with schema v1',
-  //   ['debug.pilet'],
-  //   async (ctx) => {
-  //     const port = await getFreePort(1256);
-  //     const cp = ctx.runAsync(`npx pilet debug --port ${port} --schema v1`);
+  test(
+    'debug-standard-template-with-schema-v1',
+    'can produce a debug build with schema v1',
+    ['debug.pilet'],
+    async (ctx) => {
+      const port = await getFreePort(1256);
+      const cp = ctx.runAsync(`npx pilet debug --port ${port} --schema v1`);
 
-  //     await cp.waitUntil('Ready', 'The bundling process failed');
+      await cp.waitUntil('Ready', 'The bundling process failed');
 
-  //     await page.goto(`http://localhost:${port}`);
+      await page.goto(`http://localhost:${port}`);
 
-  //     const res = await axios.get(`http://localhost:${port}/$pilet-api`);
-  //     const pilets = res.data;
+      const res = await axios.get(`http://localhost:${port}/$pilet-api`);
+      const pilets = res.data;
 
-  //     expect(pilets).toEqual({
-  //       name: expect.anything(),
-  //       version: expect.anything(),
-  //       link: expect.anything(),
-  //       spec: 'v1',
-  //       requireRef: expect.anything(),
-  //       integrity: expect.anything(),
-  //     });
+      expect(pilets).toEqual({
+        name: expect.anything(),
+        version: expect.anything(),
+        link: expect.anything(),
+        spec: 'v1',
+        requireRef: expect.anything(),
+        integrity: expect.anything(),
+      });
 
-  //     await expect(page).toHaveSelectorCount('.pi-tile', 1);
+      await expect(page).toHaveSelectorCount('.pi-tile', 1);
 
-  //     await expect(page).toMatchText('.pi-tile', 'Welcome to Piral!');
-  //   },
-  // );
+      await expect(page).toMatchText('.pi-tile', 'Welcome to Piral!');
+    },
+  );
 
-  // test(
-  //   'debug-standard-template-with-default-schema-v2',
-  //   'can produce a debug build with default schema v2',
-  //   ['debug.pilet'],
-  //   async (ctx) => {
-  //     const port = await getFreePort(1256);
-  //     const cp = ctx.runAsync(`npx pilet debug --port ${port} --schema`);
+  test(
+    'debug-standard-template-with-default-schema-v2',
+    'can produce a debug build with default schema v2',
+    ['debug.pilet'],
+    async (ctx) => {
+      const port = await getFreePort(1256);
+      const cp = ctx.runAsync(`npx pilet debug --port ${port} --schema`);
 
-  //     await cp.waitUntil('Ready', 'The bundling process failed');
+      await cp.waitUntil('Ready', 'The bundling process failed');
 
-  //     await page.goto(`http://localhost:${port}`);
+      await page.goto(`http://localhost:${port}`);
 
-  //     const res = await axios.get(`http://localhost:${port}/$pilet-api`);
-  //     const pilets = res.data;
+      const res = await axios.get(`http://localhost:${port}/$pilet-api`);
+      const pilets = res.data;
 
-  //     expect(pilets).toEqual({
-  //       name: expect.anything(),
-  //       version: expect.anything(),
-  //       link: expect.anything(),
-  //       spec: 'v2',
-  //       requireRef: expect.anything(),
-  //       dependencies: expect.anything(),
-  //     });
+      expect(pilets).toEqual({
+        name: expect.anything(),
+        version: expect.anything(),
+        link: expect.anything(),
+        spec: 'v2',
+        requireRef: expect.anything(),
+        dependencies: expect.anything(),
+      });
 
-  //     await expect(page).toHaveSelectorCount('.pi-tile', 1);
+      await expect(page).toHaveSelectorCount('.pi-tile', 1);
 
-  //     await expect(page).toMatchText('.pi-tile', 'Welcome to Piral!');
-  //   },
-  // );
+      await expect(page).toMatchText('.pi-tile', 'Welcome to Piral!');
+    },
+  );
 
-  // test(
-  //   'debug-standard-template-with-none-schema',
-  //   'can produce a debug build with none schema',
-  //   ['debug.pilet'],
-  //   async (ctx) => {
-  //     const port = await getFreePort(1256);
-  //     const cp = ctx.runAsync(`npx pilet debug --port ${port} --schema none`);
+  test(
+    'debug-standard-template-with-none-schema',
+    'can produce a debug build with none schema',
+    ['debug.pilet'],
+    async (ctx) => {
+      const port = await getFreePort(1256);
+      const cp = ctx.runAsync(`npx pilet debug --port ${port} --schema none`);
 
-  //     await cp.waitUntil('Ready', 'The bundling process failed');
+      await cp.waitUntil('Ready', 'The bundling process failed');
 
-  //     await page.goto(`http://localhost:${port}`);
+      await page.goto(`http://localhost:${port}`);
 
-  //     const res = await axios.get(`http://localhost:${port}/$pilet-api`);
-  //     const pilets = res.data;
+      const res = await axios.get(`http://localhost:${port}/$pilet-api`);
+      const pilets = res.data;
 
-  //     expect(pilets).toEqual({
-  //       name: expect.anything(),
-  //       version: expect.anything(),
-  //       link: expect.anything(),
-  //       spec: 'v0',
-  //       hash: expect.anything(),
-  //       noCache: expect.anything(),
-  //     });
+      expect(pilets).toEqual({
+        name: expect.anything(),
+        version: expect.anything(),
+        link: expect.anything(),
+        spec: 'v0',
+        hash: expect.anything(),
+        noCache: expect.anything(),
+      });
 
-  //     await expect(page).not.toHaveSelectorCount('.pi-tile', 1);
+      await expect(page).not.toHaveSelectorCount('.pi-tile', 1);
 
-  //     await expect(page).not.toMatchText('.pi-tile', 'Welcome to Piral!');
-  //   },
-  // );
+      await expect(page).not.toMatchText('.pi-tile', 'Welcome to Piral!');
+    },
+  );
 
-  // test(
-  //   'debug-standard-template-with-an-external-feed',
-  //   'can produce a debug build with an external feed',
-  //   ['debug.pilet'],
-  //   async (ctx) => {
-  //     const port = await getFreePort(1256);
-  //     const cp = ctx.runAsync(`npx pilet debug --port ${port} --feed "https://feed.piral.cloud/api/v1/pilet/sample"`);
+  test(
+    'debug-standard-template-with-an-external-feed',
+    'can produce a debug build with an external feed',
+    ['debug.pilet'],
+    async (ctx) => {
+      const port = await getFreePort(1256);
+      const cp = ctx.runAsync(`npx pilet debug --port ${port} --feed "https://feed.piral.cloud/api/v1/pilet/sample"`);
 
-  //     await cp.waitUntil('Ready', 'The bundling process failed');
+      await cp.waitUntil('Ready', 'The bundling process failed');
 
-  //     await page.goto(`http://localhost:${port}`);
+      await page.goto(`http://localhost:${port}`);
 
-  //     await expect(page).toHaveSelectorCount('.mario-tile', 1);
-  //     await expect(page).toHaveSelectorCount('.spaceshoot-tile', 1);
-  //     await expect(page).toHaveSelectorCount('.pi-tile', 1);
-  //     await expect(page).toMatchText('.pi-tile', 'Welcome to Piral!');
-  //     await expect(page).toMatchText('.mario-tile', 'Mario5');
-  //     await expect(page).toMatchText('.spaceshoot-tile', 'Spaceshoot');
-  //   },
-  // );
+      await expect(page).toHaveSelectorCount('.mario-tile', 1);
+      await expect(page).toHaveSelectorCount('.spaceshoot-tile', 1);
+      await expect(page).toHaveSelectorCount('.pi-tile', 1);
+      await expect(page).toMatchText('.pi-tile', 'Welcome to Piral!');
+      await expect(page).toMatchText('.mario-tile', 'Mario5');
+      await expect(page).toMatchText('.spaceshoot-tile', 'Spaceshoot');
+    },
+  );
 
-  // test(
-  //   'debug-standard-template-with-an-external-feed',
-  //   'can produce a debug build with an external feed',
-  //   ['debug.pilet'],
-  //   async (ctx) => {
-  //     const port = await getFreePort(1256);
-  //     const cp = ctx.runAsync(`npx pilet debug --port ${port} --feed "https://feed.piral.cloud/api/v1/pilet/sample"`);
+  test(
+    'debug-standard-template-with-an-external-feed',
+    'can produce a debug build with an external feed',
+    ['debug.pilet'],
+    async (ctx) => {
+      const port = await getFreePort(1256);
+      const cp = ctx.runAsync(`npx pilet debug --port ${port} --feed "https://feed.piral.cloud/api/v1/pilet/sample"`);
 
-  //     await cp.waitUntil('Ready', 'The bundling process failed');
+      await cp.waitUntil('Ready', 'The bundling process failed');
 
-  //     await page.goto(`http://localhost:${port}`);
+      await page.goto(`http://localhost:${port}`);
 
-  //     await expect(page).toHaveSelectorCount('.mario-tile', 1);
-  //     await expect(page).toHaveSelectorCount('.spaceshoot-tile', 1);
-  //     await expect(page).toMatchText('.mario-tile', 'Mario5');
-  //     await expect(page).toMatchText('.spaceshoot-tile', 'Spaceshoot');
-  //   },
-  // );
+      await expect(page).toHaveSelectorCount('.mario-tile', 1);
+      await expect(page).toHaveSelectorCount('.spaceshoot-tile', 1);
+      await expect(page).toMatchText('.mario-tile', 'Mario5');
+      await expect(page).toMatchText('.spaceshoot-tile', 'Spaceshoot');
+    },
+  );
 
   test(
     'debug-standard-template-with-different-app-shell',
